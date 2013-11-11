810f0a81bbca3c8f9f124b6cc413c8e7e75d4f4a
aansetiawan <aaansetiawan@gmail.com>
2013-11-11 17:58:08 +0700

    Create masuk.php

---
+++ masuk.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<title>Form Login Admin</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head> 
+<body> 
+	<form action="adminmasuk.php" method="post">
+		<label>Username:</label>
+		<input type="text" name="username" />
+		<label>Password:</label>
+		<input type="password" name="password"  />
+		<center>
+			<input type="submit" value="Login" name="submit" />
+		</center>
+		
+</form>
+</body> 
+</html>
+
