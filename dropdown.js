bf09764d0402818a6ea3bd6b575ad018e6ec3a80
22104960 <prima.2ipa1@gmail.com>
2013-11-11 18:31:43 +0700

    Create dropdown.js

---
+++ dropdown.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {			
+	$('#header_nav li').hover(
+	function () {
+	//show its submenu
+		$('ul', this).stop(true,true).slideDown(100);
+	}, 
+	function () {
+	//hide its submenu
+	$('ul', this).stop(true,true).slideUp(100);			
+	}
+	);				
+});
